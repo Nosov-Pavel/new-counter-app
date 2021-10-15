60036c32694fd5e507684f17e9edffd78bcb2d45
Nosov-Pavel <nopaev@gmail.com>
2021-10-15 06:29:43 -0400

    AddNumber

--- src/App.js
+++ src/App.js
@@ -1,43 +1,49 @@
+// import '/App.css';
 import {useState} from "react";
 
-function App(){
-    // const [numbers, setNumbers] = useState([1, 2, 3, 4, 5, 6, 7])
-    // const plusMinusCounter = (value, index) => {
-    //     const newArr = numbers.map((el, i) => index === i ? el+value: el)
-    //     setNumbers(newArr)
-    // }
-    const [numbers, senNumbers] = useState([1, 2, 3, 4, 5])
+function App() {
+    const [numbers, setNumbers] = useState([1, 2, 3, 4, 5, 6, 7])
     const plusMinusCounter = (value, index) => {
-        const newArr = numbers.map((el, i) => index === i ? el+value: el)
-    }
-    const moveUpDown = (direction, index) => {
-        const temporary = numbers[index]
-        const newArr = [...numbers]
-        newArr[index] = newArr[index+direction]
-        newArr[index+direction]=temporary
+        const newArr = numbers.map((el, i) => index === i ? el + value : el)
         setNumbers(newArr)
     }
 
-    return(
-        <div className="App">
+    const moveUpDown = (direction, index) => {
+        const temporary = numbers[index]
+        const newArr = [...numbers]
+        newArr[index] = newArr[index + direction]
+        newArr[index + direction] = temporary
+        setNumbers(newArr)
+    }
+    const addNumber = () => {
+        setNumbers([...numbers, 0])
+
+    }
+
+    return (
+        <div>
             <h1>CALCULATOR</h1>
             {numbers.map((el, index) => <div key={index}>
-                <button onClick={() => plusMinusCounter(-3, index) }>-3</button>
-                <button onClick={() => plusMinusCounter(-2, index)}>-2</button>
-                <button onClick={() => plusMinusCounter(-1, index)}>-1</button>
-                {el}
-                <button onClick={() => plusMinusCounter(1, index)}>1</button>
-                <button onClick={() => plusMinusCounter(2, index)}>2</button>
-                <button onClick={() => plusMinusCounter(3, index)}>3</button>
-                {" "}
-                <button onClick={() => moveUpDown(-1, index)} disabled={index===0}>↑</button>
-                <button onClick={() => moveUpDown(1, index)} disabled={index===numbers.length-1}>↓</button>
+                    <button onClick={() => plusMinusCounter(-3, index)}>-3</button>
+                    <button onClick={() => plusMinusCounter(-2, index)}>-2</button>
+                    <button onClick={() => plusMinusCounter(-1, index)}>-1</button>
+                    {el}
+                    <button onClick={() => plusMinusCounter(1, index)}>1</button>
+                    <button onClick={() => plusMinusCounter(2, index)}>2</button>
+                    <button onClick={() => plusMinusCounter(3, index)}>3</button>
+                    {" "}
+                    <button onClick={() => moveUpDown(-1, index)} disabled={index === 0}>↑</button>
+                    <button onClick={() => moveUpDown(1, index)} disabled={index === numbers.length - 1}>↓</button>
+                    <button>Reset</button>
+                </div>
+            )}
+            <button>AddNumber</button>
 
-            </div>)}
 
         </div>
     );
 }
+
 export default App;
 //   const [numbers, setNumbers] = useState([1, 2, 3, 4, 5])
 //
